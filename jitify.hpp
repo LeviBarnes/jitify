b00e4b64fd54c668a6f7e3248c81533185fbf101
Levi Barnes <levi@synopsys.com>
2021-02-09 19:06:49 -0800

    2 fixes for using thrust

--- jitify.hpp
+++ jitify.hpp
@@ -1410,7 +1410,7 @@ static const char* jitsafe_header_float_h = R"(
 #define DBL_MAX         1.7976931348623157e308 
 #define FLT_EPSILON     1.19209289e-7f 
 #define DBL_EPSILON     2.220440492503130e-16 
-#define FLT_MIN         1.1754943e-38f; 
+#define FLT_MIN         1.1754943e-38f
 #define DBL_MIN         2.2250738585072013e-308 
 #define FLT_ROUNDS      1
 #if defined __cplusplus && __cplusplus >= 201103L
